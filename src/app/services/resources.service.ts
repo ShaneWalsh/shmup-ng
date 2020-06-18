36431fe1027484dc6fa731c538667106ded128d8
unknown <shaneneilwalsh@gmail.com>
2020-06-18 20:21:18 +0100

    removing base64 encoder for performance

--- src/app/services/resources.service.ts
+++ src/app/services/resources.service.ts
@@ -162,7 +162,7 @@ export class ResourcesService {
     imageLoaded(code,loadedImage:HTMLImageElement){
         this.resources.set(code,loadedImage);
 		// store as global variable and then copy(temp1) to copy the object
-		this.resourcesToLoadB64.push({code:code,path:this.imgToBase64(loadedImage),type:ResourcesEnum.ImageRes})
+		//this.resourcesToLoadB64.push({code:code,path:this.imgToBase64(loadedImage),type:ResourcesEnum.ImageRes})
         if(this.resources.size == this.resourcesToLoad.length){
 			console.log("base64",this.resourcesToLoadB64);
             this.getResourcesLoaded().next(true);
